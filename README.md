7d400c787e664ab0ba725847009bcbef502ca096
Matej Fabijanić <root4unix@gmail.com>
2022-10-31 16:00:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dependencies
+
+Install Pillow
+
+Debian
+```sh
+$ sudo apt install python3-pil
+```
+
+RHEL
+```sh
+$ sudo dnf install python3-pillow
+```
+
+Python virtual environment
+```sh
+$ python3 -m pip install Pillow
+```
---
+++ find-and-resize.py
@@ -0,0 +1,70 @@
+#
+# Resize image files in directory
+#
+# Used for resize OTOBO Helpdesk images in articles that are saved on filesystem.
+# OTOBO articles directory: /opt/otobo/articles
+#
+# Author: Matej Fabijanic <root4unix@gmail.com>
+#
+
+import filetype
+import os
+import configparser
+import logging
+import logging.config
+from PIL import Image
+
+
+logging.config.fileConfig(fname='logging.ini', disable_existing_loggers=False)
+# Get the logger specified in the file
+logger = logging.getLogger('resize');
+
+config = configparser.ConfigParser()
+config.read('find-and-resize.ini')
+basewidth = int(config['image']['basewidth'])
+
+
+def resize_image(img_file, img_file_resized):
+    # fullsized image
+    img = Image.open(img_file)
+    img_width, img_hsize = img.size
+
+    # Skip if width is smaller or equal as basewidth
+    if img_width <= basewidth:
+        return logger.info('SKIP converting picture "%s"; img_width: %s' % (img_file, img_width))
+
+    wpercent = (basewidth / float(img_width))
+    hsize = int((float(img_hsize) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    logger.info('"%s" [RESIZE]-> "%s"' % (img_file, img_file_resized))
+    img.save(img_file_resized)
+    logger.info('Move "%s" -> "%s"' % (img_file_resized, img_file))
+    os.rename(img_file_resized, img_file)
+
+
+def main():
+    path = config['image']['path']
+    # temp files
+    path_tmp = config['image']['path_tmp']
+    # image formats
+    formats = [x.strip() for x in config['image']['formats'].split(',')]
+
+    if not os.path.isdir(path_tmp):
+        try:
+            os.mkdir((path_tmp))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+    for (path, dirs, files) in os.walk(path):
+        for file in files:
+            if file.endswith(tuple(formats)):
+                img_file = os.path.join(path, file)
+                if filetype.is_image(img_file):
+                    resize_image(img_file, os.path.join(path_tmp, file))
+            else:
+                logger.info('Invalid "%s"' % os.path.join(path, file))
+
+
+if __name__== "__main__":
+    main()
